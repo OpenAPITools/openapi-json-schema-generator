ca518da8be75cc2579413b5ac14accac74799f9f
William Cheng <wing328hk@gmail.com>
2022-09-25 23:12:59 +0800

    skip python-prior test

--- CI/circle_parallel.sh
+++ CI/circle_parallel.sh
@@ -61,7 +61,6 @@ elif [ "$NODE_INDEX" = "4" ]; then
 
   #mvn --no-snapshot-updates --quiet verify -Psamples.circleci.node4 -Dorg.slf4j.simpleLogger.defaultLogLevel=error
   (cd samples/openapi3/client/petstore/python && make test)
-  (cd samples/openapi3/client/petstore/python-prior && make test)
   (cd samples/openapi3/client/3_0_3_unit_test/python && make test)
 
 else
